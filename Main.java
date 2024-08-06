158f090a5bb4b879ba9238fb2ae8b656cd77f53f
Marc Isip <yougit initclear>
2024-08-06 14:35:31 -0500

    First Commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Java{
+    public static void main(String[] args){
+        /* Leet Code Question #20
+         * Easy Question #5
+        */
+        String s = "()";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s){
+        if(s.length() % 2 != 0){ // if s length is not divisible by 2 return false, cause there must always be a pair to be true
+            return false;
+        }
+
+        Stack<Character> stack = new Stack();
+        for(char c : s.toCharArray()){ // loop through each chracter in "s" String
+            if(c == '(' || c == '{' || c == '['){ // push open brackets onto the stack
+                stack.push(c);
+            
+            //If it is a corresponding closing bracket, then check if for the opening bracket, if it is pop it off, then move on
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+            } else { // if there are only closing brackets
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
